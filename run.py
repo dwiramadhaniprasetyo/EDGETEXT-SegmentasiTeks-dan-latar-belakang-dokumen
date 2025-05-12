021503478c9cb66a8632f2bc79ca79b10bd8b6f4
dwiramadhaniprasetyo <rd827534@gmail.com>
2025-05-12 13:18:39 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,9 @@
+# run.py
+import sys
+from src.app_ui import start_app  # Asumsikan kamu punya fungsi `start_app` di `app_ui.py`
+
+def main():
+    start_app()
+
+if __name__ == "__main__":
+    main()
